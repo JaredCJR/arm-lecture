0171370920ee36ea8b676e146b0a628b4375b675
JaredCJR <empire89090@gmail.com>
2015-03-02 17:01:16 +0800

    implement mutex.s as Lab-2

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -10,8 +10,16 @@
 lock_mutex:
         @ INSERT CODE BELOW
 
+	ldr r1 , =locked		@set r1 locked
+.lock_once:
+	ldrex r2,[r0]			@read the current status
+	cmp r2 , #0			@if current status is unlocked
+		strexeq r2,r1,[r0]	@set current status(r0) to be locked
+		cmpeq r2 , #0		@r2 is the condition for set lock;success -> r2==0
+		bne .lock_once		@fail to lock -> again
+
         @ END CODE INSERT
-	bx lr
+	bx lr				@return
 
 	.size lock_mutex, .-lock_mutex
 
@@ -19,9 +27,12 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
+
+	ldr r1 , =unlocked		@set r1 to be unlocked
+	str r1 , [r0]			@set current status to be unlocked
         
         @ END CODE INSERT
-	bx lr
+	bx lr				@return
 	.size unlock_mutex, .-unlock_mutex
 
 	.end
